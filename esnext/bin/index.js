73da243136b7db119265087af07d03a73f2bf677
Benjamin Lupton <b@lupton.cc>
2015-12-10 15:09:40 +0800

    boot up a temporary server

--- esnext/bin/index.js
+++ esnext/bin/index.js
@@ -2,18 +2,46 @@
 'use strict'
 const EXIT_ERROR_CODE = 1
 const app = require('../lib/app').create()
-app.init({}, function (err) {
-	if ( err ) {
-		console.error(err.stack)
-		process.exit(EXIT_ERROR_CODE)
-		return
-	}
+const server = require('../lib/server').create({log: console.log})
 
-	app.listen({}, function (err) {
+// Initialise a temporary server so heroku doesn't die waiting
+server.start({
+	middleware (req, res) {
+		res.sendError('helper still initialising')
+	},
+	next (err) {
 		if ( err ) {
 			console.error(err.stack)
 			process.exit(EXIT_ERROR_CODE)
 			return
 		}
-	})
+
+		// Initialise the App
+		app.init({}, function (err) {
+			if ( err ) {
+				console.error(err.stack)
+				process.exit(EXIT_ERROR_CODE)
+				return
+			}
+
+			// Kill our temporary server so we can boot up our application server
+			server.destroy({}, function (err) {
+				if ( err ) {
+					console.error(err.stack)
+					process.exit(EXIT_ERROR_CODE)
+					return
+				}
+
+				// Start our application server
+				app.listen({}, function (err) {
+					if ( err ) {
+						console.error(err.stack)
+						process.exit(EXIT_ERROR_CODE)
+						return
+					}
+				})
+			})
+		})
+
+	}
 })
