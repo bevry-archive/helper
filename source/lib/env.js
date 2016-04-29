da10a8e0aeba756993ac3ac7446228db6923231d
Benjamin Lupton <b@lupton.cc>
2016-04-30 03:02:56 +0800

    removed some more facebook stuff

--- source/lib/env.js
+++ source/lib/env.js
@@ -9,8 +9,7 @@ const env = {
 		twitterConsumerKey: 'BEVRY_TWITTER_CONSUMER_KEY',
 		twitterConsumerSecret: 'BEVRY_TWITTER_CONSUMER_SECRET',
 		twitterAccessToken: 'TWITTER_ACCESS_TOKEN',
-		twitterAccessTokenSecret: 'TWITTER_ACCESS_TOKEN_SECRET',
-		facebookAccessToken: 'BEVRY_FACEBOOK_APP_ACCESS_TOKEN'
+		twitterAccessTokenSecret: 'TWITTER_ACCESS_TOKEN_SECRET'
 	},
 	docpad: {
 		campaignMonitorListId: 'DP_CM_LIST_ID',
@@ -18,7 +17,6 @@ const env = {
 	},
 	startuphostel: {
 		campaignMonitorListId: 'SH_CM_LIST_ID',
-		facebookGroupId: 'SH_FACEBOOK_GROUP_ID',
 		apiKey: 'SH_API_KEY'
 	}
 }
